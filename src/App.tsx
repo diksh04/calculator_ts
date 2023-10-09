78e0eaec02a315aa40fcc3a121e31d412131ac83
Dikshant Luthra <dikshantluthra922@gmail.com>
2023-10-09 13:34:58 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { numbers } from "./constants";
+const operator: string[] = ["+", "-", "/", "*"];
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+const App = () => {
+  // const [] = useState<{ name: string; age: number }>({ name: "", age: 0 });
+  // const [state, setState] = useState<Person>({ name: "", age: 0 });
+  const [inp, setInput] = useState<string>("0");
 
-function App() {
+  const clickHandler = (number: string) => {
+    try {
+      if (inp[0] == "0") {
+        setInput(number);
+      } else if (inp[0] === "*") {
+        setInput("0");
+      } else setInput((prevState) => prevState + number);
+    } catch (e) {
+      toast.error("Invalid Operation,Kindly input valid operation!");
+    }
+  };
+  const clearHandler = () => {
+    setInput("0");
+  };
+  const ansHandler = () => {
+    try {
+      setInput(eval(inp));
+      toast.success("Calculation successful!");
+    } catch (e) {
+      toast.error("Invalid Operation,Kindly input valid operation!");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="inp"
+        type="text"
+        value={inp}
+        onChange={(event) => setInput(event.target.value)}
+      ></input>
+
+      <div className="options">
+        {numbers.map((val, idx) => (
+          <button
+            key={val}
+            className={`btn  ${idx % 4 === 3 ? "btnspl" : ""} `}
+            onClick={() => clickHandler(`${val}`)}
+          >
+            {val}
+          </button>
+        ))}
+
+        <button className="btn" onClick={clearHandler}>
+          AC
+        </button>
+        <button className=" btn btnsplspl" onClick={ansHandler}>
+          =
+        </button>
+      </div>
+      <ToastContainer />
     </div>
   );
-}
+};
 
 export default App;
